e9d863257f798fe5f5fad69d8168d40220fa0af8
Jackson Donovan <iinventedtwitter@gmail.com>
2017-08-06 14:31:18 -0600

    env port added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
-const port = 3000
+const port = 3000 || process.env.PORT;
 
 app.set('view engine', 'html')
 app.use(express.static(path.join(__dirname, 'public')));
