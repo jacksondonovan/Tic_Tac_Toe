1952e97a030df8ecab992a3401f7a41f0551025e
Jackson Donovan <iinventedtwitter@gmail.com>
2017-08-06 00:36:20 -0600

    dev works

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const port = 3000
+
+app.set('view engine', 'html')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.get('/',(req,res)=>{
+  res.render('index')
+})
+
+app.listen(port,(req,res)=>{
+  console.log('listening on port ' + port)
+})
